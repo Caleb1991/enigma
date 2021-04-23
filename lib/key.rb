9a2db566f17cfcade54524e6828fb6b255e13e02
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-23 09:21:13 -0600

    Create date transformation

--- lib/key.rb
+++ lib/key.rb
@@ -1,18 +1,20 @@
 class Key
-  attr_reader :library,
+  attr_reader :alphabet,
               :random_number,
               :a_shift,
               :b_shift,
               :c_shift,
-              :d_shift
+              :d_shift,
+              :date
 
-  def initialize
-    @library = ('a'..'z').to_a << ' '
+  def initialize(date)
+    @date = date
+    @alphabet = ('a'..'z').to_a << ' '
     @random_number = random_number_generator
-    @a_shift = shift[0].sum + date_transformation(date)[0]
-    @b_shift = shift[1].sum + date_transformation(date)[1]
-    @c_shift = shift[2].sum + date_transformation(date)[2]
-    @d_shift = shift[3].sum + date_transformation(date)[3]
+    @a_shift = shift[0].sum + date_transformation[0]
+    @b_shift = shift[1].sum + date_transformation[1]
+    @c_shift = shift[2].sum + date_transformation[2]
+    @d_shift = shift[3].sum + date_transformation[3]
   end
 
   def random_number_generator
@@ -25,5 +27,10 @@ class Key
     pairs = []
     @random_number.each_cons(2) { |a| pairs << a}
     pairs
+  end
+
+  def date_transformation
+    new = @date.to_i ** 2
+    new.to_s[-4..-1].to_i
   end
 end
