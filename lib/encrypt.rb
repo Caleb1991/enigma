45db0e95299f784ec92ddf5a71094a5a1f625afc
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-25 17:43:15 -0600

    Make corrections on puts statement

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,7 +5,7 @@ enigma = Enigma.new
 
 phrase = File.open(ARGV[0], 'r')
 
-this = enigma.encrypt(phrase.read)
+encryption = enigma.encrypt(phrase.read)
 
 phrase.close
 
@@ -15,4 +15,4 @@ writer.write(enigma.encoded_phrase)
 
 writer.close
 
-puts "Created #{ARGV[1]} with the key #{this[:encryption]} and date #{this[:date]}"
+puts "Created #{ARGV[1]} with the key #{encryption[:key]} and date #{encryption[:date]}"
