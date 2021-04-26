8505449fb9502870440b164f47217fc646442181
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-26 13:35:16 -0600

    Create edge case tests for encrypt and decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -11,6 +11,11 @@ class Enigma
   end
 
   def encrypt(phrase, key = random_key_string, date = today)
+    if key.length != 5
+      'Please input five integers'
+    elsif date.length != 6
+      'Please input date as MMDDYY'
+    else
     encoded = Encoder.new(phrase, key, date)
     @encoded_phrase = encoded.encoded_phrase
 
@@ -19,17 +24,24 @@ class Enigma
         key: key,
         date: date
       }
+    end
   end
 
   def decrypt(phrase, key, date = today)
-    decoded = Decoder.new(phrase, key, date)
-    @decoded_phrase = decoded.decoded_phrase
+    if key.length != 5
+      'Please input five integers'
+    elsif date.length != 6
+      'Please input date as MMDDYY'
+    else
+      decoded = Decoder.new(phrase, key, date)
+      @decoded_phrase = decoded.decoded_phrase
 
-    decryption = {
-      decryption: decoded.decoded_phrase,
-      key: key,
-      date: date
-    }
+      decryption = {
+        decryption: decoded.decoded_phrase,
+        key: key,
+        date: date
+      }
+    end
   end
 
   def create_range
